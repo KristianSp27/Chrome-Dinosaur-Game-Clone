5fb33eadbcc56ed8508634d3ab0e85810fea6b3a
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-03-30 23:40:07 +0300

    Update

---
+++ script.js
@@ -0,0 +1,14 @@
+const WORLD_WIDTH = 100;
+const WORLD_HEIGHT = 30;
+
+const worldElem = document.querySelector("[data-world]");
+
+setPixelToWorldScale();
+window.addEventListener("resize", setPixelToWorldScale);
+
+function setPixelToWorldScale() {
+  let worldToPixelScale;
+  if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+    worldToPixelScale = window.innerWidth / WORLD_WIDTH;
+  }
+}
