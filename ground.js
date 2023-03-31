9be9df159cb9526e50423a8772ff057cdc7d9c79
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-03-31 14:28:42 +0300

    Update

--- ground.js
+++ ground.js
@@ -1,7 +1,12 @@
-import { incrementCustomProperty } from "./updateCustomProperty";
+import { incrementCustomProperty, setCustomProperty } from "./updateCustomProperty";
 
 const SPEED = 0.05;
 const groundElems = document.querySelectorAll("[data-ground]");
+
+export function setUpGround() {
+  setCustomProperty(groundElems[0], "--left", 0);
+  setCustomProperty(groundElems[1], "--left", 300);
+}
 
 export function updateGround(delta) {
   groundElems.forEach((ground) => {
