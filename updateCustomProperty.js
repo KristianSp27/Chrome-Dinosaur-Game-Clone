d9cedf0a7a7349ab79a3157b729b122cc3d4917e
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-03-31 14:25:30 +0300

    Update

--- updateCustomProperty.js
+++ updateCustomProperty.js
@@ -2,6 +2,10 @@ export function getCustomProperty(elem, prop) {
   return parseFloat(getComputedStyle(elem).getPropertyValue(prop)) || 0;
 }
 
-export function setCustomProperty() {}
+export function setCustomProperty(elem, prop, value) {
+  elem.style.setProperty(prop, value);
+}
 
-export function incrementCustomProperty() {}
+export function incrementCustomProperty(elem, prop, inc) {
+  setCustomProperty(elem, prop, getCustomProperty(elem, prop) + inc);
+}
