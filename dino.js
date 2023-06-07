a50d888a19d2fbe8edf1b2da1c4dedc9b89e4e57
kristian <kristian@betixon.com>
2023-06-07 23:13:03 +0300

    Update

---
+++ dino.js
@@ -0,0 +1,23 @@
+const dinoElem = document.querySelector("[data-dino]");
+const JUMP_SPEED = .45;
+const GRAVITY = .011;
+const DINO_FRAME_COUNT = 2;
+const FRAME_TIME = 100;
+
+
+export function setUpDino() {
+    
+}
+
+export function updateDino(delta, speedScale) {
+    handleRun();
+    handleJump();
+}
+
+function handleRun() {
+    
+}
+
+function handleJump() {
+    
+}
