8a7ba1918a1d969a32f02f089d774b30eed2dd68
kristian <kristian.spasov27@gmail.com>
2023-06-07 23:21:49 +0300

    Update

--- dino.js
+++ dino.js
@@ -1,23 +1,28 @@
 const dinoElem = document.querySelector("[data-dino]");
-const JUMP_SPEED = .45;
-const GRAVITY = .011;
+const JUMP_SPEED = 0.45;
+const GRAVITY = 0.011;
 const DINO_FRAME_COUNT = 2;
 const FRAME_TIME = 100;
 
-
+let isJumping;
 export function setUpDino() {
-    
+  isJumping = false;
 }
 
 export function updateDino(delta, speedScale) {
-    handleRun();
-    handleJump();
+  handleRun(delta, speedScale);
+  handleJump();
 }
 
-function handleRun() {
-    
+function handleRun(delta, speedScale) {
+  if (isJumping) {
+    dinoElem.src = `imgs/dino-stationary.jpg`;
+    return;
+  }
+
+  if (currentFrameTime > FRAME_TIME) {
+  }
+  currentFrameTime += delta * speedScale;
 }
 
-function handleJump() {
-    
-}
+function handleJump() {}
