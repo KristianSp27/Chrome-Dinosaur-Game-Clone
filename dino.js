4e1a4191107ce5a64b74d79f4cbab66d5c2d03bd
kristian <kristian.spasov27@gmail.com>
2023-06-07 23:26:31 +0300

    Update

--- dino.js
+++ dino.js
@@ -6,9 +6,11 @@ const FRAME_TIME = 100;
 
 let isJumping;
 let dinoFrame;
+let currentFrameTime;
 export function setUpDino() {
   isJumping = false;
   dinoFrame = 0;
+  currentFrameTime = 0;
 }
 
 export function updateDino(delta, speedScale) {
@@ -18,12 +20,14 @@ export function updateDino(delta, speedScale) {
 
 function handleRun(delta, speedScale) {
   if (isJumping) {
-    dinoElem.src = `imgs/dino-stationary.jpg`;
+    dinoElem.src = `imgs/dino-stationary.png`;
     return;
   }
 
   if (currentFrameTime > FRAME_TIME) {
     dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT;
+    dinoElem.src = `imgs/dino-run-${dinoFrame}.png`;
+    currentFrameTime -= FRAME_TIME;
   }
   currentFrameTime += delta * speedScale;
 }
