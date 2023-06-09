b96bf2a1c581d2319c6b4e724589d1d13d0b1860
kristian <kristian.spasov27@gmail.com>
2023-06-09 23:52:06 +0300

    Update

--- cactus.js
+++ cactus.js
@@ -1,4 +1,4 @@
-import { getCustomProperty, incrementCustomProperty, setCustomProperty } from "./updateCustomProperty.js";
+import { setCustomProperty, incrementCustomProperty, getCustomProperty } from "./updateCustomProperty.js";
 
 const SPEED = 0.05;
 const CACTUS_INTERVAL_MIN = 500;
@@ -8,12 +8,15 @@ const worldElem = document.querySelector("[data-world]");
 let nextCactusTime;
 export function setupCactus() {
   nextCactusTime = CACTUS_INTERVAL_MIN;
+  document.querySelectorAll("[data-cactus]").forEach((cactus) => {
+    cactus.remove();
+  });
 }
 
-export function updateCactus() {
+export function updateCactus(delta, speedScale) {
   document.querySelectorAll("[data-cactus]").forEach((cactus) => {
     incrementCustomProperty(cactus, "--left", delta * speedScale * SPEED * -1);
-    if (getCustomProperty(cactus, "--left") <= 100) {
+    if (getCustomProperty(cactus, "--left") <= -100) {
       cactus.remove();
     }
   });
@@ -23,6 +26,12 @@ export function updateCactus() {
     nextCactusTime = randomNumberBetween(CACTUS_INTERVAL_MIN, CACTUS_INTERVAL_MAX) / speedScale;
   }
   nextCactusTime -= delta;
+}
+
+export function getCactusRects() {
+  return [...document.querySelectorAll("[data-cactus]")].map((cactus) => {
+    return cactus.getBoundingClientRect();
+  });
 }
 
 function createCactus() {
@@ -35,5 +44,5 @@ function createCactus() {
 }
 
 function randomNumberBetween(min, max) {
-  Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
