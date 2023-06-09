25466ef752e957ee0455b4abe23e10a2b22c2af1
kristian <kristian.spasov27@gmail.com>
2023-06-09 23:41:31 +0300

    Update

--- cactus.js
+++ cactus.js
@@ -1,7 +1,14 @@
 const SPEED = 0.05;
 const CACTUS_INTERVAL_MIN = 500;
 const CACTUS_INTERVAL_MAX = 2000;
+const worldElem = document.querySelector("[data-world]");
 
-export function setupCactus() {}
+let nextCactusTime;
+export function setupCactus() {
+  nextCactusTime = CACTUS_INTERVAL_MIN;
+}
 
-export function updateCactus() {}
+export function updateCactus() {
+  if (nextCactusTime <= 0) {
+  }
+}
